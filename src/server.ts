ae09676dbd5dca39d849a13eefd15046d6c7678a
GuFvr <gustavo.vfavero@gmail.com>
2025-02-20 00:51:11 -0300

    feat(server): register referral tracking routes

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,10 @@ import {
 } from 'fastify-type-provider-zod'
 import { env } from './env'
 
-import { accessInviteLinkRoute } from './routes/access-invite-link'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -39,6 +42,9 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
